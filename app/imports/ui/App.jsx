d097cf834df277882eb64e117f3e3f6c2173c4cb
KentHB <156981653+KentHB@users.noreply.github.com>
2024-03-02 10:43:58 -1000

    Section 1A

---
+++ app/imports/ui/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Hello } from './Hello.jsx';
+import { Info } from './Info.jsx';
+
+export const App = () => (
+  <div>
+    <h1>Welcome to Meteor!</h1>
+    <h2>Test refresh</h2>
+    <Hello/>
+    <Info/>
+  </div>
+);
